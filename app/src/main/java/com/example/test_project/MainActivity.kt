20ec75e1ff2260d7897708c57d97eff3b543424d
Jiy-park <jiyong3954@gmail.com>
2023-01-30 16:22:10 +0900

    슬라이딩 패널, 위에서 슬라이드하면 창 올라옴 참고 : https://mechacat.tistory.com/18

--- app/src/main/java/com/example/test_project/MainActivity.kt
+++ app/src/main/java/com/example/test_project/MainActivity.kt
@@ -4,16 +4,21 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.annotation.Dimension
+import androidx.annotation.Dimension.DP
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.test_project.databinding.ActivityMainBinding
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import render.animations.Attention
 import render.animations.Bounce
 import render.animations.Render
@@ -27,31 +32,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val render = Render(this@MainActivity)
-        var mItem_list = mutableListOf<String>()
-        mItem_list.run {
-            add("박지용")
-            add("이현진")
-            add("으아")
+        val slidePanel = binding.mainFrame                      // SlidingUpPanel
+
+
+        // 터치로 슬라이드 가능 여부 설정 (panelState 변경으로 여닫는 건 가능)
+        binding.btnTouch.setOnClickListener {
+            val touchEnabled = slidePanel.isTouchEnabled
+            if (touchEnabled) {
+                binding.btnTouch.text = "잠금해제"
+                slidePanel.isTouchEnabled = false
+            } else {
+                binding.btnTouch.text = "터치잠금"
+                slidePanel.isTouchEnabled = true
+            }
         }
 
-        val adapter = ViewPagerAdapter(this@MainActivity)
-        adapter.item_list = mItem_list
-
-        binding.viewPager.adapter = adapter
-        binding.viewPager.offscreenPageLimit = 3
-        binding.run {
-            btnAdd.setOnClickListener {
-                if(edName.text.toString() == ""){
-                    render.setAnimation(Attention().Bounce(edName))
-                    render.setDuration(500L)
-                    render.start()
-                }
-                else{
-                    adapter.add_item(edName.text.toString())
-                    edName.text.clear()
-                    Toast.makeText(this@MainActivity, "item_list size : ${mItem_list.size}",Toast.LENGTH_SHORT).show()
-                }
+        // 패널 활성화 여부 설정 (터치, 함수 모두 불가능)
+        binding.btnEnable.setOnClickListener {
+            val enabled = slidePanel.isEnabled
+            if (enabled) {
+                binding.btnEnable.text = "활성화"
+                slidePanel.isEnabled = false
+            } else {
+                binding.btnEnable.text = "비활성화"
+                slidePanel.isEnabled = true
             }
         }
 
